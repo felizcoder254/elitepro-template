94ea841183093b010ad2f56a676311ef4be81dc1
felizcoder254 <felizxcoder@gmail.com>
2025-12-25 13:29:54 +0300

    Create Kernel.php

---
+++ app/Http/Kernel.php
@@ -0,0 +1,8 @@
+protected $middleware = [
+    \App\Http\Middleware\TrustProxies::class,  // ← ADD THIS LINE
+    \Illuminate\Http\Middleware\HandleCors::class,
+    \App\Http\Middleware\PreventRequestsDuringMaintenance::class,
+    \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
+    \App\Http\Middleware\TrimStrings::class,
+    \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
+];
