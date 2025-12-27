054f66f980233eb99d1fd5e2e1165f0931080058
felizcoder254 <felizxcoder@gmail.com>
2025-12-27 15:28:40 +0300

    Update app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,6 +4,13 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
+$_ENV['SESSION_SECURE_COOKIE'] = 'false';
+$_ENV['SESSION_SAME_SITE'] = 'none';
+$_ENV['SESSION_DOMAIN'] = '.onrender.com';
+putenv('SESSION_SECURE_COOKIE=false');
+putenv('SESSION_SAME_SITE=none');
+putenv('SESSION_DOMAIN=.onrender.com');
+
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
